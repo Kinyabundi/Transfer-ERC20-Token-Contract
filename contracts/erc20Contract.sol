cc7659c227dd81ac38a326d4fd077c48b3225bb4
Kinyabundi <kinyachristine14@gmail.com>
2024-03-29 13:50:23 +0300

    added openzeppelin

--- contracts/erc20Contract.sol
+++ contracts/erc20Contract.sol
@@ -1,18 +1,8 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.9;
 
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Basic is IERC20 {
     string public constant name = "ERC20Basic";
